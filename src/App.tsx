7b725ed3b7edb979a1b6bca457e19ce94b33aa8e
Nick <hello@nicholasg.me>
2022-08-04 22:56:04 +0100

    chore: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
+import { Box, Flex, Heading, Image, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Flex h="600px" pos="relative" direction="column" p="5vw" pt={['150px', null, null, '100px', '150px']} bgImg="/KimberleyBg.png" bgRepeat="no-repeat" bgSize="cover" bgPosition="top" backdropFilter="blur(5px)" backgroundColor="rgba(0,0,0,0.6)" align="center" justify="center" maxH="100vh" gap={10}>
+        <Box h="640px" w="100%" pos="absolute" bottom={0} left={0} backdropFilter="blur(5px)" bgColor="rgba(255,255,255,0.2)" zIndex={-1} />
+        <Image alignSelf="flex-start" src="/kimberley_logo.png" pos="absolute" top={5} height={110} width={110} alt="Logo" />
+        <Heading color="brand.kimberley" as="h1" size="3xl" fontWeight="bold">Kimbo BPhO</Heading>
+        <Heading w="80%" minW="300px">Our entry for the maiden BPhO Computational Physics Challenge - 2022</Heading>
+        <Heading w="80%" minW="300px">By Jamie Whiting and Nicholas Gregory</Heading>
+        </Flex>
+      </>
+
+      <Flex flexFlow="column nowrap" align="center">
+        <Box minW="300px" w="80%" mt={5} borderRadius="5px" border="1px solid gray">
+          <Tabs>
+            <TabList>
+              <Tab>Task 1</Tab>
+              <Tab>Task 2</Tab>
+              <Tab>Task 3</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <Heading as="h2" size="lg" fontWeight="bold">Task 1</Heading>
+              </TabPanel>
+              <TabPanel>
+                <Heading as="h2" size="lg" fontWeight="bold">Task 2</Heading>
+              </TabPanel>
+              <TabPanel>
+                <Heading as="h2" size="lg" fontWeight="bold">Task 3</Heading>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Flex>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import theme from 'util/theme'
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+      </ChakraProvider>
   </React.StrictMode>
 );
