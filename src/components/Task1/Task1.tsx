0029ab3c89db7c3ef664b7a352f958b1c00a708d
Jacques <jamie.whiting2003@gmail.com>
2022-08-07 16:17:11 +0100

    task 1 data

--- src/components/Task1/Task1.tsx
+++ src/components/Task1/Task1.tsx
@@ -1,18 +1,27 @@
 import { Heading } from "@chakra-ui/react";
-import { Line, LineChart } from "recharts";
+import { Scatter, ScatterChart, XAxis, YAxis, CartesianGrid, ZAxis } from "recharts";
 
-// const series = [
-//   {
-//     name: 
-//   }
-// ]
+const data = [
+  {x: 0, y: 288},
+  {x: 11, y: 216.5},
+  {x: 20, y: 216.5},
+  {x: 32, y: 228.5},
+  {x: 47, y: 270.5},
+  {x: 51, y: 270.5},
+  {x: 71, y: 214.5},
+  {x: 80, y: 196.5}
+];
 
 const Task1 = () => (
   <>
     <Heading as="h2" size="lg" fontWeight="bold">Task 1</Heading>
-    <LineChart width={500} height={500}>
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-    </LineChart>
+    <ScatterChart width={500} height={500}>
+      <CartesianGrid/>
+      <XAxis type="number" dataKey="x" name="Altitude" unit="km"/>
+      <YAxis type="number" dataKey="y" name="Temperature" unit="K" domain={[200,300]}/>
+      <ZAxis type="number" range={[20]}/>
+      <Scatter name="TvA" data={data} fill="#711368" line/>
+    </ScatterChart>
   </>
 )
 
