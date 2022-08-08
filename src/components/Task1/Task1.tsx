a87488bbcac02bd733e89b587628fb842f478d92
Nick <hello@nicholasg.me>
2022-08-08 13:48:05 +0100

    feat: add explanation buttons

--- src/components/Task1/Task1.tsx
+++ src/components/Task1/Task1.tsx
@@ -1,7 +1,10 @@
-import { Heading, Flex, Box } from '@chakra-ui/react';
+import {
+  Heading, Flex, Box, useDisclosure, Button,
+} from '@chakra-ui/react';
 import {
   Scatter, ScatterChart, XAxis, YAxis, CartesianGrid, ZAxis, ResponsiveContainer,
 } from 'recharts';
+import Task1Modal from './Modal';
 
 const data = [
   { x: 0, y: 288 },
@@ -14,22 +17,29 @@ const data = [
   { x: 80, y: 196.5 },
 ];
 
-const Task1 = () => (
-  <Flex flexFlow="column nowrap" align="center">
-    <Heading as="h2" size="lg" fontWeight="bold">Task 1</Heading>
+const Task1 = () => {
+  const modalDisc = useDisclosure();
+  return (
+    <>
+      <Flex flexFlow="column nowrap" align="center">
+        <Heading as="h2" size="lg" fontWeight="bold">Task 1</Heading>
+        <Button alignSelf={[null, 'flex-end']} pos="relative" top={[0, -9]} onClick={modalDisc.onOpen}>Explanation</Button>
 
-    <Box maxW="500px" width="100%">
-      <ResponsiveContainer minWidth="300px" width="100%" aspect={1}>
-        <ScatterChart>
-          <CartesianGrid />
-          <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
-          <YAxis type="number" dataKey="y" name="Temperature" unit="K" domain={[200, 300]} />
-          <ZAxis type="number" range={[20]} />
-          <Scatter name="TvA" data={data} fill="#711368" line />
-        </ScatterChart>
-      </ResponsiveContainer>
-    </Box>
-  </Flex>
-);
+        <Box maxW="500px" width="100%">
+          <ResponsiveContainer minWidth="300px" width="100%" aspect={1}>
+            <ScatterChart>
+              <CartesianGrid />
+              <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
+              <YAxis type="number" dataKey="y" name="Temperature" unit="K" domain={[200, 300]} />
+              <ZAxis type="number" range={[20]} />
+              <Scatter name="TvA" data={data} fill="#711368" line />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </Box>
+      </Flex>
+      <Task1Modal disclosure={modalDisc} />
+    </>
+  );
+};
 
 export default Task1;
--- src/components/Task3/Task3.tsx
+++ src/components/Task3/Task3.tsx
@@ -1,11 +1,12 @@
 import {
-  Heading, Select, Spinner, Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark, Flex,
+  Heading, Select, Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark, Flex, useDisclosure, Button,
 } from '@chakra-ui/react';
 import { useMemo, useState } from 'react';
 import {
   Scatter, ScatterChart, XAxis, YAxis, CartesianGrid, ZAxis, ResponsiveContainer,
 } from 'recharts';
 import { rk4, euler } from 'wasm';
+import Task3Modal from './Modal';
 
 const xValues = new Array(111).fill(0).map((_, i) => i * 0.1);
 
@@ -34,6 +35,7 @@ const Task3 = () => {
   // const [currentData, setCurrentData] = useState<Float32Array | null>(null);
   const [switchState, setSwitchState] = useState<'rk4' | 'euler'>('rk4');
   const [humidity, setHumidity] = useState<number>(0.5);
+  const modalDisc = useDisclosure();
 
   const currentData: Float32Array = useMemo(() => {
     if (switchState === 'rk4') {
@@ -42,15 +44,12 @@ const Task3 = () => {
     return euler(humidity);
   }, [switchState, humidity]);
 
-  const data: Data | null = useMemo(() => {
-    if (currentData) {
-      const vals: Data = { p: [], t: [], l: [] };
-      xValues.forEach((value, index) => vals.p.push({ x: value, y: currentData[index] }));
-      xValues.forEach((value, index) => vals.t.push({ x: value, y: currentData[index + 111] }));
-      xValues.forEach((value, index) => vals.l.push({ x: value, y: currentData[index + 222] }));
-      return vals;
-    }
-    return null;
+  const data = useMemo(() => {
+    const vals: Data = { p: [], t: [], l: [] };
+    xValues.forEach((value, index) => vals.p.push({ x: value, y: currentData[index] }));
+    xValues.forEach((value, index) => vals.t.push({ x: value, y: currentData[index + 111] }));
+    xValues.forEach((value, index) => vals.l.push({ x: value, y: currentData[index + 222] }));
+    return vals;
   }, [currentData]);
 
   // setEulerData(euler(0.5)); // Pressure/Temperature/Lapse-rate (each 1101) 0-11km in step sizes of 0.01 -> CHANGED TO EACH 111 IN STEP SIZES OF 0.1
@@ -62,74 +61,70 @@ const Task3 = () => {
   // Either find a way to make correct step sizes or have another state object that we change when the switch is changed
   // One state object { p: Float32Array, t: Float32Array, l: Float32Array }
 
-  if (!currentData || !data) {
-    return (
-      <>
-        <Heading as="h2" size="lg" fontWeight="bold">Task 3</Heading>
-        <Spinner size="xl" />
-      </>
-    );
-  }
-
   return (
-    <Flex flexFlow="column nowrap" align="center" gap={5}>
-      <Heading as="h2" size="lg" fontWeight="bold">Task 3</Heading>
+    <>
+      <Flex flexFlow="column nowrap" align="center">
+        <Heading as="h2" size="lg" fontWeight="bold">Task 3</Heading>
+        <Button alignSelf={[null, 'flex-end']} pos="relative" top={[0, -9]} onClick={modalDisc.onOpen}>Explanation</Button>
 
-      <Flex flexFlow="column nowrap" align="center" w="100%">
-        <Heading as="h3" size="md" fontWeight="bold">Calculation Method</Heading>
-        <Select value={switchState} onChange={(e) => setSwitchState(e.currentTarget.value as 'rk4' | 'euler')} maxWidth="300px" my={3}>
-          <option value="rk4">RK4</option>
-          <option value="euler">Euler</option>
-        </Select>
-      </Flex>
+        <Flex flexFlow="column nowrap" align="center" w="100%">
+          <Heading as="h3" size="md" fontWeight="bold">Calculation Method</Heading>
+          <Select value={switchState} onChange={(e) => setSwitchState(e.currentTarget.value as 'rk4' | 'euler')} maxWidth="300px" my={3}>
+            <option value="rk4">RK4</option>
+            <option value="euler">Euler</option>
+          </Select>
+        </Flex>
 
-      <Flex flexFlow="column nowrap" align="center" w="100%">
-        <Heading as="h3" size="md" fontWeight="bold">Humidity</Heading>
-        <Slider aria-label="slider-ex-1" value={humidity} onChange={(val) => setHumidity(val)} max={1} step={0.01} minWidth="250px" width="60%" my={3}>
-          {new Array(11).fill(0).map((_, i) => (
+        <Flex flexFlow="column nowrap" align="center" w="100%">
+          <Heading as="h3" size="md" fontWeight="bold">Humidity</Heading>
+          <Slider aria-label="slider-ex-1" value={humidity} onChange={(val) => setHumidity(val)} max={1} step={0.01} minWidth="250px" width="60%" my={3}>
+            {new Array(11).fill(0).map((_, i) => (
             // eslint-disable-next-line react/no-array-index-key
-            <SliderMark visibility={i % 2 === 0 ? 'visible' : ['hidden', null, 'visible']} key={i} value={i * 0.1} {...labelStyles}>{(i * 0.1).toFixed(1)}</SliderMark>
-          ))}
-          <SliderTrack>
-            <SliderFilledTrack />
-          </SliderTrack>
-          <SliderThumb />
-        </Slider>
+              <SliderMark visibility={i % 2 === 0 ? 'visible' : ['hidden', null, 'visible']} key={i} value={i * 0.1} {...labelStyles}>{(i * 0.1).toFixed(1)}</SliderMark>
+            ))}
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+        </Flex>
+
+        <Flex flexFlow="row wrap" justify="center" w="100%" maxW="1500px" mt={3}>
+          <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
+            <ScatterChart>
+              <CartesianGrid />
+              <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
+              <YAxis type="number" dataKey="y" name="Pressure" unit="K" />
+              <ZAxis type="number" range={[20]} />
+              <Scatter name="TvA" data={data.p} fill="#711368" line />
+            </ScatterChart>
+          </ResponsiveContainer>
+
+          <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
+            <ScatterChart>
+              <CartesianGrid />
+              <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
+              <YAxis type="number" dataKey="y" name="Temperature" unit="K" />
+              <ZAxis type="number" range={[20]} />
+              <Scatter name="TvA" data={data.t} fill="#711368" line />
+            </ScatterChart>
+          </ResponsiveContainer>
+
+          <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
+            <ScatterChart>
+              <CartesianGrid />
+              <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
+              <YAxis type="number" dataKey="y" name="Lapse Rate" unit="K" />
+              <ZAxis type="number" range={[20]} />
+              <Scatter name="TvA" data={data.l} fill="#711368" line />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </Flex>
+
       </Flex>
 
-      <Flex flexFlow="row wrap" justify="center" w="100%" maxW="1500px" mt={3}>
-        <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
-          <ScatterChart>
-            <CartesianGrid />
-            <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
-            <YAxis type="number" dataKey="y" name="Pressure" unit="K" />
-            <ZAxis type="number" range={[20]} />
-            <Scatter name="TvA" data={data.p} fill="#711368" line />
-          </ScatterChart>
-        </ResponsiveContainer>
-
-        <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
-          <ScatterChart>
-            <CartesianGrid />
-            <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
-            <YAxis type="number" dataKey="y" name="Temperature" unit="K" />
-            <ZAxis type="number" range={[20]} />
-            <Scatter name="TvA" data={data.t} fill="#711368" line />
-          </ScatterChart>
-        </ResponsiveContainer>
-
-        <ResponsiveContainer minWidth="300px" width="33%" aspect={1}>
-          <ScatterChart>
-            <CartesianGrid />
-            <XAxis type="number" dataKey="x" name="Altitude" unit="km" />
-            <YAxis type="number" dataKey="y" name="Lapse Rate" unit="K" />
-            <ZAxis type="number" range={[20]} />
-            <Scatter name="TvA" data={data.l} fill="#711368" line />
-          </ScatterChart>
-        </ResponsiveContainer>
-      </Flex>
-
-    </Flex>
+      <Task3Modal disclosure={modalDisc} />
+    </>
   );
 };
 
