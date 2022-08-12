cb584bbc67c6bcf23bcb8ecde2d6104e50217ea3
Nick <hello@nicholasg.me>
2022-08-12 15:23:59 +0100

    feat: add technical report

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,14 @@ function App() {
     <div className="App">
       <Flex h="600px" pos="relative" direction="column" p="5vw" pt={['150px', null, null, '100px', '150px']} bgImg="/KimberleyBg.png" bgRepeat="no-repeat" bgSize="cover" bgPosition="top" backdropFilter="blur(5px)" backgroundColor="rgba(0,0,0,0.6)" align="center" justify="center" maxH="100vh" gap={10}>
         <Box h="640px" w="100%" pos="absolute" bottom={0} left={0} backdropFilter="blur(5px)" bgColor="rgba(255,255,255,0.2)" zIndex={-1} />
+
         <Image alignSelf="flex-start" src="/kimberley_logo.png" pos="absolute" top={5} height={110} width={110} alt="Logo" />
-        <Link pos="absolute" top={70} right={50} fontSize="18" fontWeight="bold" isExternal href="https://github.com/KimboBPhO/Website">GitHub <ExternalLinkIcon mt="-5px" /></Link>
+
+        <Flex pos="absolute" right={50} top={50} gap={3} maxW="calc(100vw - 150px)" flexFlow="row wrap" justify="flex-end">
+          <Link fontSize="18" fontWeight="bold" isExternal href="/Report.pdf">Technical Report <ExternalLinkIcon mt="-5px" /></Link>
+          <Link fontSize="18" fontWeight="bold" isExternal href="https://github.com/KimboBPhO/Website">GitHub <ExternalLinkIcon mt="-5px" /></Link>
+        </Flex>
+
         <Heading color="brand.kimberley" as="h1" size="3xl" fontWeight="bold">Kimbo BPhO</Heading>
         <Heading w="80%" minW="300px">Our entry for the maiden BPhO Computational Physics Challenge - 2022</Heading>
         <Heading w="80%" minW="300px">By Jamie Whiting and Nicholas Gregory</Heading>
